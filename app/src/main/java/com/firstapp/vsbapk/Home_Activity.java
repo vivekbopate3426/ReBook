df7432b6d417c09303a3b977f934da50a3775fb3
Vivek Bopate <vivekbopate3@gmail.com>
2023-01-31 09:26:05 +0530

    First commit 23/01/20023 Second commit 24/01/2023 3rd commit 28/01/2023 4th commit 31/01/2023

---
+++ app/src/main/java/com/firstapp/vsbapk/Home_Activity.java
@@ -0,0 +1,17 @@
+package com.firstapp.vsbapk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Home_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        getSupportActionBar().hide();
+    }
+}
--- app/src/main/java/com/firstapp/vsbapk/MainActivity.java
+++ app/src/main/java/com/firstapp/vsbapk/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -15,16 +16,26 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        final ProgressBar progressBar= findViewById(R.id.welcome_progressbar);
         mAuth = FirebaseAuth.getInstance();
         FirebaseUser user = mAuth.getCurrentUser();
         final TextView name = findViewById(R.id.Welcome_name);
-        name.setText("Welcome Back-" + user.getDisplayName());
-        if (user != null) {
+        if (user!= null ) {
+            name.setText("Welcome Back-" + user.getDisplayName());
+            progressBar.setVisibility(View.GONE);
+            Intent intent = new Intent(MainActivity.this, Home_Activity.class);
+            startActivity(intent);
         } else {
             Intent intent = new Intent(MainActivity.this, Login_Activity.class);
+            progressBar.setVisibility(View.GONE);
             startActivity(intent);
 
         }
+
     }
+
+
 
 }
