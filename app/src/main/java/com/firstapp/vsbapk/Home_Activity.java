cc378c33818dedfb431f6b0b8ea155c7f97bbda3
Vivek Bopate <vivekbopate3@gmail.com>
2023-02-01 22:06:49 +0530

    First commit 23/01/20023 Second commit 24/01/2023 3rd commit 28/01/2023 4th commit 31/01/2023 5th commit 01/02/2023

--- app/src/main/java/com/firstapp/vsbapk/Home_Activity.java
+++ app/src/main/java/com/firstapp/vsbapk/Home_Activity.java
@@ -1,10 +1,13 @@
 package com.firstapp.vsbapk;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+
+import com.firstapp.vsbapk.fragment.Home_Fragment;
 
 public class Home_Activity extends AppCompatActivity {
 
@@ -13,5 +16,8 @@ public class Home_Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         getSupportActionBar().hide();
+        getSupportFragmentManager()
+                .beginTransaction()
+                .commit();
     }
 }
